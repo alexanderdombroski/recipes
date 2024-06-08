8f34788cc4311f560a217e3b6e1e453124b5d414
alexanderdombroski <dom21013@byui.edu>
2024-06-08 11:19:26 -0600

    api request class can now use pagigation to navigate the giant response

--- src/fetch_recipes.ts
+++ src/fetch_recipes.ts
@@ -1,6 +1,8 @@
 class apiReciever {
     // Attributes
-    private readonly url: string;
+    private url: string;
+    private prevPage: Stack<string> = new Stack<string>;
+    private nextPage: string = "";
 
     // Methods
     public constructor(query: string | null = null, health: string[] = [], cuisine: string[] = [], meals: string[] = [], dishTypes: string[] = []) {
@@ -25,17 +27,54 @@ class apiReciever {
         return url;
     }
 
-    public async query(): Promise<any> {
-        const response = await fetch(this.url)
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error(`HTTP request failed. Status: ${response.status}`);
-                }
-                return response.json();
-            })
-            .catch(error => {
-                console.error(`Could not make HTTP request: Error: ${error}`);
-                throw error;
-            });
+    private async query(url: string | null = null): Promise<any> {
+        try {
+            const response = await fetch(url ? url : this.url);
+            if (!response.ok) {
+                throw new Error(`HTTP request failed. Status: ${response.status}`);
+            }
+            const json =  await response.json();
+            this.nextPage = (json["_links"]?.["next"]?.["href"] as string) ?? "";
+        } catch (error) {
+            console.error(`Could not make HTTP request: Error: ${error}`);
+            throw error;
+        }  
+    }
+
+    // Accessble Query Functions
+    public queryFirst(): Promise<any> | null{
+        try {
+            return this.query();
+        } catch {
+            console.log("Problem encountered in API query, returning null")
+            return null
+        }
+    }
+    public queryNext(): Promise<any> | null {
+        if (this.pageRight()) {
+            this.prevPage.push(this.url);
+            this.url = this.nextPage;
+            return this.query();
+        } else {
+            console.log("No more pages to query");
+            return null;
+        }
+    }
+    public queryPrev(): Promise<any> | null {
+        if (this.pageLeft()) {
+            this.url = this.prevPage.pop() ?? "";
+            return this.query()
+        } else {
+            console.log("No prevous pages to query");
+            return null;
+        }
+    }
+
+    // Used to confirm that there are left/right pages
+    public pageLeft(): boolean {
+        return !this.prevPage.isEmpty()
+    }
+    public pageRight(): boolean {
+        return this.nextPage === "" ? false : true
     }
 }
